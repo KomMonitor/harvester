7c385baccc37d9bf72fa953315ceda31f60dcb1a
cDanowski <christian.danowski-buhren@hs-bochum.de>
2021-07-29 09:14:10 +0200

    imple,ent a workaround to support CORS and configure express middleware

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ require('dotenv').config();
 
 var path = require('path');
 var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var oas3Tools = require('oas3-tools');
 var serverPort = process.env.PORT || 8089;
+// var serverPort = 8089;
 
 // swaggerRouter configuration
 var options = {
@@ -16,15 +20,30 @@ var options = {
 };
 
 var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
-var app = expressAppConfig.getApp();
 
-var cors = require('cors');
+var openApiApp = expressAppConfig.getApp();
+
+/*
+    workaround to support CORS and modify some express middleware parameters
+*/
+
+const app = express();
+
+app.use(bodyParser.json({limit:'50mb'}));
+app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}));
+
 const corsOptions = {
-    // exposedHeaders: 'Access-Control-Allow-Origin,Location,Connection,Content-Type,Date,Transfer-Encoding'
-    exposedHeaders: ['Access-Control-Allow-Origin','Location','Connection','Content-Type','Date','Transfer-Encoding','Origin','X-Requested-With', 'Accept'],
-    origin: "*"
-  };
-  app.use(cors(corsOptions));
+  // exposedHeaders: 'Access-Control-Allow-Origin,Location,Connection,Content-Type,Date,Transfer-Encoding'
+  exposedHeaders: ['Access-Control-Allow-Origin','Location','Connection','Content-Type','Date','Transfer-Encoding','Origin','X-Requested-With', 'Accept'],
+  origin: "*"
+};
+
+// Add headers
+app.use(/.*/, cors(corsOptions));
+
+for (let i = 2; i < openApiApp._router.stack.length; i++) {
+    app._router.stack.push(openApiApp._router.stack[i])
+}
 
 // Initialize the Swagger middleware
 http.createServer(app).listen(serverPort, function () {
